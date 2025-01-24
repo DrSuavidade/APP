a728a3872d889375c74f74936a4f0dc56979f2f5
Davis0uza <58854976+Davis0uza@users.noreply.github.com>
2025-01-24 19:23:10 +0000

    Fixed Blank pages, check intall
    npm install react-router-dom

--- frontendweb/src/App.js
+++ frontendweb/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import AppRoutes from './routes/AppRoutes'; // Certifica-te que o caminho está correto
+import AppRoutes from './routes/AppRoutes'; // Certifica-te de que o caminho está correto
 
 function App() {
   return (
     <div className="App">
+      {/* Invocar as rotas existentes */}
       <AppRoutes />
     </div>
   );
