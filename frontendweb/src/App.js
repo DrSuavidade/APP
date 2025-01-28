167180257210cfa56ba518cbf10bec4e2f1759dc
afonsombpl7 <pv22631@alunos.estgv.ipv.pt>
2025-01-28 23:02:40 +0000

    Alterações nas páginas

--- frontendweb/src/App.js
+++ frontendweb/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './components/Navbar'; // Garante que o caminho esteja correto
 import './App.css';
 import AppRoutes from './routes/AppRoutes'; // Certifica-te de que o caminho está correto
 
@@ -8,8 +9,12 @@ function App() {
       <div className="page-container">
         {/* Invocar as rotas existentes */}
         <AppRoutes />
+      <div>
+          <Navbar />
+          {/* Outras páginas/conteúdo abaixo */}
       </div>
-    </div>
+      </div>
+      </div>
   );
 }
 
