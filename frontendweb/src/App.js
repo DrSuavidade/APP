5bc50d8c7ea9bcbd1f91eae5b380cc656499aea7
afonsombpl7 <pv22631@alunos.estgv.ipv.pt>
2025-01-30 01:51:03 +0000

    Conclusão da página LOGIN e Registar + limpeza no css

--- frontendweb/src/App.js
+++ frontendweb/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import Navbar from './components/Navbar'; // Garante que o caminho esteja correto
 import './App.css';
-import AppRoutes from './routes/AppRoutes'; // Certifica-te de que o caminho está correto
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
       <div className="page-container">
-        {/* Invocar as rotas existentes */}
-        <AppRoutes />
-      <div>
-          <Navbar />
-          {/* Outras páginas/conteúdo abaixo */}
+        <AppRoutes /> {/* Mantemos apenas o AppRoutes */}
       </div>
-      </div>
-      </div>
+    </div>
   );
 }
 
