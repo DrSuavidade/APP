a66eaa828ee5fe0395fce1c2e8f4a148edc3e8e0
MarcoSSantos49 <pv23195@alunos.estgv.ipv.pt>
2025-02-01 20:40:33 +0000

    Component - EventList conectado a bd

--- frontendweb/src/components/EventList.jsx
+++ frontendweb/src/components/EventList.jsx
@@ -1,12 +1,25 @@
-const EventoList = ({ jogo, escalão, data, hora, local }) => {
+import React from "react";
+import "../CSS/EventList.css";
+
+const EventoList2 = ({ events }) => {
   return (
-    <div className="bg-gray-800 p-4 rounded-lg text-white shadow-md">
-      <p className="text-sm">Scouter: Marco Santos</p>
-      <h3 className="font-bold">{jogo}</h3>
-      <p className="text-sm">{escalão} | {data} | {hora}</p>
-      <p className="text-sm flex items-center"><span className="mr-2">📍</span>{local}</p>
+    <div className="evento-list2">
+      {events.map((event, index) => (
+        <div key={index} className="evento-item">
+          <div className="evento-teams">
+            <p className="home-team">{event.homeTeam}</p>
+            <p className="away-team">{event.awayTeam}</p>
+          </div>
+          <p className="scouter">{event.scouter}</p>
+          <p className="date">{event.date}</p>
+          <p className="time">{event.time}</p>
+          <p className="location">
+            <span className="material-icons"></span> {event.location}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default EventoList;
+export default EventoList2;
