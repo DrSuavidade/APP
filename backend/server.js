977ca38d1d8e387f6ccdaa92fb3ea30b6f2f0a40
DrSuavidade <rafazercoconascalcas4@gmail.com>
2025-02-05 16:05:57 +0000

    Updated server.js for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,8 @@ app.get('/', (req, res) => {
 });
 
 // Iniciar servidor
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
